04636710bcb2cb6984bf15aff5c5328319342bc3
kryptn <kryptn@gmail.com>
2021-05-23 00:18:17 -0600

    max rep required only looks at augmentations not owned

--- faction.js
+++ faction.js
@@ -1,20 +1,30 @@
 function getMaxRep(ns, fac) {
-    return Math.max(...ns.getAugmentationsFromFaction(fac).map(aug => ns.getAugmentationCost(aug)[0]));
+    const owned = ns.getOwnedAugmentations();
+
+
+    return Math.max(...ns.getAugmentationsFromFaction(fac).filter(aug => !owned.includes(aug)).map(aug => ns.getAugmentationCost(aug)[0]));
 }
 
 
 async function work(ns, fac, target) {
-    while(ns.getFactionRep(fac) < target) {
+    while (ns.getFactionRep(fac) < target) {
         ns.workForFaction(fac, "hacking");
-        await ns.sleep(60*1000);
+        await ns.sleep(60 * 1000);
     }
 }
 
 export async function main(ns) {
 
-    const factions = ["Tian Di Hui", "NiteSec", "Chongqing", "New Tokyo", "Ishima"];
+    const factions = [
+        //"Tian Di Hui",
+        //"NiteSec",
+        "Chongqing",
+        //"New Tokyo",
+        //"Ishima",
+        "BitRunners"
+    ];
 
-    for(const faction of factions) {
+    for (const faction of factions) {
         // ns.tprint(ns.getAugmentationsFromFaction(faction));
 
         const currentRep = ns.getFactionRep(faction);
